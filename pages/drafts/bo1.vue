c1d1b8c9bf5e0782a865efc0d1d5a8e616186373
koreanpanda345 <koreanpanda345@gmail.com>
2020-09-30 15:46:40 -0700

    Added a netlify.toml to handle redirects to the error page

--- pages/drafts/bo1.vue
+++ pages/drafts/bo1.vue
@@ -18,8 +18,8 @@
   </div>
   <div>
         <h1 class="text-white text-xl px-20 place-content-center flex flex-center">Season 7</h1>
-        <a class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="/error">Minors</a>
-        <a class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="/error">Majors</a>
+        <a class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="/drafts/leagues/season7/bo1/minors">Minors</a>
+        <a class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="/drafts/leagues/season7/bo1/majors">Majors</a>
   </div>
 </div>
 </template>
