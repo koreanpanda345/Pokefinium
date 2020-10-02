22fe7f8cc5436327cd24284281189ce1a313f13c
koreanpanda345 <koreanpanda345@gmail.com>
2020-10-02 09:40:44 -0700

    Changing back to the button for th ptr till i enable some stuff

--- pages/fakemons/ptr.vue
+++ pages/fakemons/ptr.vue
@@ -5,14 +5,14 @@
         This allows us to not only show what we been doing, but it allows the public to find bugs, and stuff that we didn't.
         </h3>
         <h3 class="text-3xl">Due to Our hosting server, not Allowing us to actually host our servers 24/7, The last 10 Days of each Month The server will not be supported by our hosting service. Once we launch our server, we will moving this to a better hosting service.</h3>
-        <iframe
+        <!-- <iframe
         src="http://pokefinium-public-testing.herokuapp.com/testclient.html?~~ptr-server-igl.herokuapp.com:80"
         frameborder="0"
         onmousewheel=""
         width="100%"
         height= "533"
-        style="background: transparent; border: 1px solid #ccc;"></iframe>
-        <!-- <a class="no-underline text-center inline-block text-sm  py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="http://pokefinium-public-testing.herokuapp.com/testclient.html?~~ptr-server-igl.herokuapp.com:80">Click here to go to the PTR!</a> -->
+        style="background: transparent; border: 1px solid #ccc;"></iframe> -->
+        <a class="no-underline text-center inline-block text-sm  py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0" href="http://pokefinium-public-testing.herokuapp.com/testclient.html?~~ptr-server-igl.herokuapp.com:80">Click here to go to the PTR!</a>
         <iframe
         class="airtable-embed"
         src="https://airtable.com/embed/shrLpI8cGJNLx1B0d?backgroundColor=green"
