25c3206b2c4f971866c9e12f5fa0d7d67f9970ae
koreanpanda345 <koreanpanda345@gmail.com>
2020-10-04 15:44:52 -0700

    testing page with Porygon Bot's kills api

---
+++ pages/drafts/replays.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+		<h1>Replays were made, and tracked by Porygon Bot!</h1>
+		<p v-if="$fetchState.pending">Fetching Data...</p>
+		<p v-else-if="$fetchState.error">An Error Occured</p>
+		<h2 v-else>{{replayData}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    replayData: {}
+  }),
+  async fetch () {
+    this.replayData = await fetch('https://kills.porygonbot.xyz/sports-gen8nationaldexdlc2legacy-167800').then(res => res.text())
+  }
+}
+</script>
